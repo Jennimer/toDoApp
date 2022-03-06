747253108c3784a649d4ad3b0442dc51728f0180
Jennimer <92848029+Jennimer@users.noreply.github.com>
2022-03-06 16:41:14 +0200

    Update appi.js

--- appi.js
+++ appi.js
@@ -1,149 +1,167 @@
+const date = document.getElementById('paiva');
+const options = { weekday: "long", month: "short", day: "numeric" }
+const today = new Date();
+date.innerHTML = today.toLocaleDateString("en-FI", options);
 
 
-const lista = document.getElementById("lista");
-const uudetTekemiset = document.getElementById("uudetTekemiset");
-const lisaaNappi = document.getElementsByTagName("button")[0];
+
+// vars
+let id = 0;
+const lista = document.querySelector('#lista');
+const lisaaNappi = document.querySelector('#lisaaTekeminen');
+
+// UI Class
+class UI {
+  //display todos
+  static naytaToDo() {
+
+    const tehtavat = Store.lataaToDos();
+    tehtavat.forEach((tehtava) => UI.lisaaToDoToLista(tehtava.teksti, tehtava.id, tehtava.valmis));
+  }
+
+  // adding  ToDo To List
+  static lisaaToDoToLista(tehtava, id, ifChecked) {
+    // 
+
+    console.log("lisaa");
+    const valmis = ifChecked ? 'checked' : '';
+    const status = ifChecked ? 'fa-check-square' : 'fa-square';
+    const listanAsia = `<li>
+        <i class="fa-regular ${status} tehty" action="valmis" id="${id}"></i>
+        <p class="text ${valmis}">${tehtava}</p>
+        <i class="far fa-solid fa-trash-can" action="poista" id="${id}"></i>
+        </li>`;
+    const position = "beforeend";
+    lista.insertAdjacentHTML(position, listanAsia);
+  }
+
+  // removing element
+  static poistaToDo(elementti) {
+    // removing item from UI
+    elementti.parentNode.parentNode.removeChild(elementti.parentNode);
+
+
+    // get value of the current id and remove it from storage
+    const curId = elementti.attributes.id.value;
+    const tehtavat = Store.lataaToDos();
+    tehtavat.forEach((tehtava, indeksi) => {
+
+      if (+tehtava.id === +curId) {
+        console.log("poistaa")
+        tehtavat.splice(indeksi, 1);
+      }
+    });
+
+    localStorage.setItem('tehtava', JSON.stringify(tehtavat));
+  }
+
+  // complete element
+  static merkkaaToDo(elementti) {
+    const CHECK = 'fa-check-square';
+    const UNCHECK = 'fa-square';
+    elementti.classList.toggle(CHECK);
+    elementti.classList.toggle(UNCHECK);
+    elementti.parentNode.querySelector(".text").classList.toggle("checked");
+
+    // update the storage
+    const curId = elementti.attributes.id.value;
+    const tehtavat = Store.lataaToDos();
+    console.log(curId);
+    tehtavat.forEach((tehtava, indeksi) => {
+      console.log(tehtava.id);
+      if (+tehtava.id === +curId) {
+        console.log("on");
+        tehtavat[indeksi].valmis = tehtavat[indeksi].valmis ? false : true;
+      }
+
+    });
+    localStorage.setItem('tehtava', JSON.stringify(tehtavat));
+
+  }
 
 
 
-var lisaaTehtava = function(){
-        const teksti = uudetTekemiset.value;
-        if (teksti == "" || teksti == "Add task") {
-            alert("Add new task!")
-             
-        }
-        if(teksti.length > 20){
-            alert("Task too long");
-            uudetTekemiset.value = "Add Task";
-            return
-        }
-        var tekemiset = uudetTekemiset.value;
-        
-    
-        
-        var listanAsiat = luoUusiTehtava(tekemiset);
-        lista.appendChild(listanAsiat);
-        uudetTekemiset.value = "Add task";
-        console.log(lista);
-        saveTasksToLocalStorage();
-        
-        
+  // clear all todo
+  static poistaKaikkiToDo() {
+    console.log("poistaakaikki")
+    lista.innerHTML = '';
+    localStorage.clear();
+  }
+
+}
+
+// store class
+class Store {
+  static lataaToDos() { //loading task to the local
+    console.log("lataa");
+    let tehtavat;
+    if (localStorage.getItem('tehtava') === null) {   
+      tehtavat = [];    
+    } else {
+      tehtavat = JSON.parse(localStorage.getItem('tehtava'));
     }
-    var luoUusiTehtava = function(tekemiset){
-    
+    return tehtavat;
+  }
 
-        var listanTehtavat = document.createElement("li");
-        var tekoDiv = document.createElement("label");
-        var editInput = document.createElement("input");
-        var poistaNappi = document.createElement("button1");
-        var editNappi = document.createElement("button2");
-        var merkkausNappi = document.createElement("button3");
-        
-        listanTehtavat.className = "text";
-        poistaNappi.className = "fa-solid fa-trash-can";
-        editNappi.className = "fa-solid fa-pen";
-        editInput.type = "text";
-        merkkausNappi.className = "fa-solid fa-check"
-        
-        
-        tekoDiv.innerText = tekemiset;
-        
+  static lisaaToDoToLista(tehtava, id) {
+    console.log("lisaa");
+    const tehtavat = Store.lataaToDos();
+
+    tehtavat.push({ teksti: tehtava, id: id, valmis: false });
+
+    localStorage.setItem('tehtava', JSON.stringify(tehtavat));
+  }
+}
+
+// Event to display todos
+document.addEventListener('DOMContentLoaded', UI.naytaToDo);
+
+// if click lisaaNappi then we call 
+// addNewTodo from UI
+lisaaNappi.addEventListener("click", function (e) {
+  
+  if (e.target.id == "lisaaTekeminen") {
     
-        listanTehtavat.appendChild(merkkausNappi);
-        listanTehtavat.appendChild(tekoDiv); 
-        listanTehtavat.appendChild(editInput);
-        listanTehtavat.appendChild(editNappi);
-        listanTehtavat.appendChild(poistaNappi);
-        
+    const uudetTekemisetInput = document.querySelector("#tekemisetInput");
     
-        poistaNappi.onclick = poistaTehtavat;
-        merkkausNappi.onclick = viivaaTehtavat;
-        editNappi.onclick = editoiTehtava;
-        
-    
-        return listanTehtavat;
+    if (uudetTekemisetInput.value == "") {
+      alert("Add new task");
+      return
     }
-  var poistaTehtavat = function(){ 
-    var listanTehtavat = this.parentNode;
-    
-   lista.removeChild(listanTehtavat);
-    saveTasksToLocalStorage();
-}
+    if (uudetTekemisetInput.value.length > 20) {
+      alert("Task too long!");
+      uudetTekemisetInput.value = "";
+      return
+    }
+    else {
+      // add to do to UI
+      UI.lisaaToDoToLista(uudetTekemisetInput.value, Date.now());
 
-var viivaaTehtavat = function(){
-
-        var teko = this.parentNode;
-        var viivattava = teko.querySelector("label");
-        console.log(viivattava);
-        
-        if(viivattava.className === "checked")
-        {
-            
-            viivattava.classList.remove("checked");
-            
-            saveTasksToLocalStorage();
-            return;}
-    
-        else {       
-            viivattava.classList.add("checked");
-            
-            saveTasksToLocalStorage();
-            
-            
-        }
-    
-
-}
-var editoiTehtava = function(){
-    console.log("editoi...");
-    var tehtava = this.parentNode;
-    var laatikko = tehtava.querySelector("input[type=text]");
-    var label = tehtava.querySelector("label");
-    var editLuokka = tehtava.classList.contains("editMode");
-
-     if(editLuokka){
-        label.innerText=laatikko.value;
-
-        }
-     else{
-         laatikko.value=label.innerText;
-     }
-     tehtava.classList.toggle("editMode");
-
-}
-
-        
-    
-    
-
-
-    function loadTaskFromLocalStorage() {
-        var tallennetut = localStorage.getItem("tehtavat");
-        tallennetut = JSON.parse(tallennetut);
-        if (!tallennetut){
-            return
-        }
-    
-        
-        for(var i = 0; i < tallennetut.length; i++){
-            var tehtava = tallennetut[i];
-            var listanTehtavat = luoUusiTehtava(tehtava);
-            lista.appendChild(listanTehtavat);
-        }
-    
-}
-function saveTasksToLocalStorage(){
-    var toDos = [];
-    var taskItems = lista.querySelectorAll(".text");
-    for(var i = 0; i < taskItems.length; i++){
-        var listaTehtavat = taskItems[i];
-        var label = listaTehtavat.querySelector("label").textContent;
-        
-        toDos.push(label)
+      // add todo to local storage
+      Store.lisaaToDoToLista(uudetTekemisetInput.value, Date.now());
+      uudetTekemisetInput.value = "";
+      // increment id
+      id++;
     }
     
-    localStorage.setItem("tehtavat", JSON.stringify(toDos));
-}
+  }
+});
 
-loadTaskFromLocalStorage();
-lisaaNappi.addEventListener("click", lisaaTehtava);
+// checking and removing items
+lista.addEventListener("click", (event) => {
+
+  const elementti = event.target;
+  if (elementti.attributes.action) {
+    const elementtiToiminta = elementti.attributes.action.value;
+    if (elementtiToiminta == "valmis") {
+      UI.merkkaaToDo(elementti);
+    } else if (elementtiToiminta == "poista") {
+      UI.poistaToDo(elementti);
+    }
+    else if (elementtiToiminta == "edit") {
+      UI.editoiToDo(elementti)
+    }
+  }
+
+});
+
